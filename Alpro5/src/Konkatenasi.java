f006f3eaec18a55ed976e88f71bd23e15cd776ad
rafaalfarhezy77 <rafaalfarhezy@student.uns.ac.id>
2025-12-11 20:29:36 +0700

    gini loh

---
+++ Alpro5/src/Konkatenasi.java
@@ -0,0 +1,8 @@
+public class Konkatenasi {
+    public static void main(String[] args) {
+        String namabuah = "Mangga";
+        int harga       = 15000;
+        
+        System.out.println("Harga " + namabuah + " = Rp " + harga);
+    }
+}
---
+++ Alpro5/src/LebaranKeluaran.java
@@ -0,0 +1,11 @@
+
+public class LebaranKeluaran {
+    public static void main(String[] args) {
+        int harga = 21750;
+        
+        System.out.printf("%8d\n", harga);
+        System.out.printf("%9d\n", harga);
+        System.out.printf("%10d\n", harga);
+        System.out.printf("%11d\n", harga);
+    }
+}
